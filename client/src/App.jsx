2ba38b94268075a2292eb0e1d1b1c57ca605235f
Olen Dale Pinto <olenpinto97@gmail.com>
2025-04-30 17:20:12 +0530

    Emp List

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import EmployeeForm from './components/EmployeeForm';
+import EmployeeList from './components/EmployeeList';
 
 function App() {
   return (
     <div>
       <EmployeeForm />
+      <EmployeeList />
     </div>
   );
 }
